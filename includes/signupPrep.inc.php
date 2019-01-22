8fc7e1b0f21c70af0b5dfc609e1654accc69e8f6
Lachlan Mackenzie <lachlan.mack9999@gmail.com>
2019-01-22 22:07:25 +1100

    Added further error handling and example of password hashing.

--- includes/signupPrep.inc.php
+++ includes/signupPrep.inc.php
@@ -13,25 +13,35 @@
 
     // if statement to check that all variables are not empty. If any of them are empty than user will be sent back to the index page.
     if (empty($first) || empty($last) || empty($email) || empty($uid) || empty($pwd)) {
-      header("Location: ../index.php?signup=error");
+      header("Location: ../prepStatInsert.php?signup=empty");
+      exit();
     } else {
-      // if statement to check if the email address is valid. Will send the user back to the index page if not valid.
-      if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        header("Location: ../index.php?signup=invalidemail");
+      // if statement to check for invalid characters in the first and last name variables.
+      if (!preg_match("/^[a-zA-Z]*$/", $first) || !preg_match("/^[a-zA-Z]*$/", $last)) {
+        header("Location: ../prepStatInsert.php?signup=char&email=$email&uid=$uid");
+        exit();
       } else {
-        $sql = "INSERT INTO users (user_first, user_last, user_email, user_uid, user_pwd) VALUES (?, ?, ?, ?, ?);";
-        $stmt = mysqli_stmt_init($conn);
-        
-        if (!mysqli_stmt_prepare($stmt, $sql)) {
-          // Sends the user back to the index page
-          header("Location: ../index.php?signup=error");
+        // if statement to check if the email address is valid. Will send the user back to the index page if not valid.
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+          header("Location: ../prepStatInsert.php?signup=invalidemail&first=$first&last=$last&uid=$uid");
+          exit();
         } else {
-          mysqli_stmt_bind_param($stmt, "sssss", $first, $last, $email, $uid, $pwd);
-          mysqli_stmt_execute($stmt);
+          $sql = "INSERT INTO users (user_first, user_last, user_email, user_uid, user_pwd) VALUES (?, ?, ?, ?, ?);";
+          $stmt = mysqli_stmt_init($conn);
+          
+          if (!mysqli_stmt_prepare($stmt, $sql)) {
+            // Sends the user back to the index page
+            header("Location: ../prepStatInsert.php?signup=sqlerror");
+            exit();
+          } else {
+            mysqli_stmt_bind_param($stmt, "sssss", $first, $last, $email, $uid, $pwd);
+            mysqli_stmt_execute($stmt);
 
-          header("Location: ../index.php?signup=success"); // loads the index page upon success.
+            header("Location: ../prepStatInsert.php?signup=success"); // loads the index page upon success.
+            exit();
+          }
         }
-      }
+      }   
     } 
   }
 ?>
